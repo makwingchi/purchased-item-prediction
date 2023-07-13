371acb5424b165f5f7e62e65ca436d5a37167ecb
leyangchai <leyangchai@whale.im>
2023-07-13 12:23:51 +0800

    post process

--- post_process.py
+++ post_process.py
@@ -3,6 +3,8 @@ import os
 import pandas as pd
 from config import get_configurations
 
+THRESHOLD = 0.5
+
 if __name__ == '__main__':
     config = get_configurations()
     processed_test_path = config['runner']['processed_test']
@@ -11,5 +13,5 @@ if __name__ == '__main__':
     infer_df = pd.read_csv('./infer.csv')
     test_data['pred'] = infer_df['pred']
 
-    re_data = test_data[test_data['pred'] >= 0.5]
+    re_data = test_data[test_data['pred'] >= THRESHOLD]
     re_data.to_csv('./u2i.csv', header=False)
--- postprocess.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-THRESHOLD = 0.5
-
-if __name__ == "__main__":
-    test = pd.read_csv('./process_test/data.csv')
-    tmp = pd.read_csv('./infer.csv')
-    test['pred'] = tmp['pred']
-    test[test['pred'] > THRESHOLD][['user_id', 'goods_id']].to_csv('./u2i.csv', index=False)
